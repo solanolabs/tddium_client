8d1e1ad7bc23606eb43f27e51683b9d745cd43c5
William Josephson <wkj@solanolabs.com>
2015-06-12 14:11:26 +0000

    Update client gem version.

--- lib/tddium_client/version.rb
+++ lib/tddium_client/version.rb
@@ -1,5 +1,5 @@
-# Copyright (c) 2011, 2012, 2013, 2014 Solano Labs All Rights Reserved
+# Copyright (c) 2011, 2012, 2013, 2015 Solano Labs All Rights Reserved
 
 module TddiumClient
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 end
